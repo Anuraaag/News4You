21f061295e193176bdeb3c8843e5a6556ece6b17
Anuraaag <anuraggupta10000@gmail.com>
2022-03-10 05:34:55 +0530

    data fetched from api, prev-next added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export class Navbar extends Component {
       <div>
           
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <a className="navbar-brand" href="#">News 4 You</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card">
-                    <img className="card-img-top" src={imageUrl} alt="Card image cap"/>
+                    <img className="card-img-top" src={imageUrl} alt=""/>
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
-                            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary px-3">Read More</a>
                         </div>
                 </div>
             </div>
